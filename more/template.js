18b1395620d27cdfd436d9b8c8a08a1ae2128bfc
gemasajaa <gemalagifrominfinitydreams@gmail.com>
2023-06-21 20:35:33 +0700

    fixed

--- more/template.js
+++ more/template.js
@@ -91,7 +91,7 @@ module.exports = {
 							align-items:center;
 							justify-content:space-between;
 							background:white;
-							box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 5px, rgba(0, 0, 0, 0.14) 0px 2px 2px, rgba(0, 0, 0, 0.12) 0px 3px 1px -2px;
+							border:2px solid lightgray;
 						}
 						.insideleft span img{
 							width:100%;
